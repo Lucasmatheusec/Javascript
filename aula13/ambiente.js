49839ae093314776d13a19e25ca67d25c6fcda7a
Lucas Matheus <138264742+Lucasmatheusec@users.noreply.github.com>
2023-07-13 21:19:10 -0300

    Repetições

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 6){
+
+}
+
+for(inicio ; teste ; incremento){
+    
+}
+/*var c = 1
+while (c < 6){
+    console.log('Tudo bem?')
+    c++
+}while é igual a enquanto*/
